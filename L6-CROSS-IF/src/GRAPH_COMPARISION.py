219e8d699da7c8effbfc8a9ab4f85982feb5181b
SANGEETA <sangeeta.69b1@gmail.com>
2017-01-09 15:39:29 +0530

    crossif

--- L6-CROSS-IF/src/GRAPH_COMPARISION.py
+++ L6-CROSS-IF/src/GRAPH_COMPARISION.py
@@ -27,7 +27,7 @@ path = "E:\\Sangeeta\\Research\\L6-CROSS-IF\\result\\"
 #==========================================================================#
 plt.close()
 
-impr_LF_logoptplus_using_clif_nb=(8.15, 7.16, 6.29, 2.8, 8.18, 4.34, -2.67, -6.76)
+avg_impr_LF_logoptplus_using_clif_nb=(8.15, 7.16, 6.29, 2.8, 8.18, 4.34, -2.67, -6.76)
 
 ind = np.arange(8)
 width = 0.3
@@ -38,7 +38,7 @@ fig, ax = plt.subplots()
 # add some text for labels, title and axes ticks
 plt.rcParams.update({'font.size': 15})
 #ylim(0,20)
-ax.set_ylabel('Improvement in LF (%)')
+ax.set_ylabel('Average Improvement in LF (%)')
 ax.set_xlabel("$CLIF_{NB}$ Classifier")
 ax.set_title('Improvement in LF (%)')
 ax.set_xticks(ind+width)
@@ -47,13 +47,13 @@ ax.set_xticklabels(('ADA', 'ADT', 'BN','J48', 'LOG',  'NB','RF', 'SVM'), rotatio
 #==== Command to change font size of legend ===#
 fontP = FontProperties()
 fontP.set_size('small')
-rects1 = ax.bar(ind, impr_LF_logoptplus_using_clif_nb, width,color ='#bebebe')
+rects1 = ax.bar(ind, avg_impr_LF_logoptplus_using_clif_nb, width,color ='#bebebe')
 ax.plot([0., 7], [0, 0], "k-")
 
 plt.tight_layout()
 #plt.show()
 
-plt.savefig(path+"clif_nb_impr_lf.pdf")
+plt.savefig(path+"clif-nb-avg-impr-lf.pdf")
 
 
 
@@ -63,7 +63,7 @@ plt.savefig(path+"clif_nb_impr_lf.pdf")
 #==========================================================================#
 plt.close()
 
-impr_ra_logoptplus_using_clif_nb=(4.89, 4.11, -1.65, -6.65, 4.15,-0.37, -1.43, -6.24)
+avg_impr_ra_logoptplus_using_clif_nb=(4.89, 4.11, -1.65, -6.65, 4.15,-0.37, -1.43, -6.24)
 
 ind = np.arange(8)
 width = 0.3
@@ -76,20 +76,20 @@ plt.rcParams.update({'font.size': 15})
 ylim(-15,15)
 ax.set_ylabel('Improvement in RA (%)')
 ax.set_xlabel("$CLIF_{NB}$ Classifier")
-ax.set_title('Improvement in RA (%)')
+ax.set_title('Average Improvement in RA (%)')
 ax.set_xticks(ind+width)
 ax.set_xticklabels(('ADA', 'ADT', 'BN','J48', 'LOG',  'NB','RF', 'SVM'), rotation = 300)
 
 #==== Command to change font size of legend ===#
 fontP = FontProperties()
 fontP.set_size('small')
-rects1 = ax.bar(ind, impr_ra_logoptplus_using_clif_nb, width,color ='#bebebe')
+rects1 = ax.bar(ind, avg_impr_ra_logoptplus_using_clif_nb, width,color ='#bebebe')
 ax.plot([0., 7], [0, 0], "k-")
 
 plt.tight_layout()
 #plt.show()
 
-plt.savefig(path+"clif_nb_impr_ra.pdf")
+plt.savefig(path+"clif-nb-avg-impr-ra.pdf")
 
 
 
@@ -100,7 +100,7 @@ plt.savefig(path+"clif_nb_impr_ra.pdf")
 #==========================================================================#
 plt.close()
 
-impr_LF_logoptplus_using_clif_bn=(7.66, 8.21,3.01, 8.16, 2.08, -2.06, -8.92)
+avg_impr_LF_logoptplus_using_clif_bn=(7.66, 8.21,3.01, 8.16, 2.08, -2.06, -8.92)
 
 #@NOTE: BN is removed because it was not working on sone dataset  because of low probablity
 
@@ -114,20 +114,20 @@ plt.rcParams.update({'font.size': 15})
 #ylim(0,20)
 ax.set_ylabel('Improvement in LF (%)')
 ax.set_xlabel("$CLIF_{BN}$ Classifier")
-ax.set_title('Improvement in LF (%)')
+ax.set_title('Average Improvement in LF (%)')
 ax.set_xticks(ind+width)
 ax.set_xticklabels(('ADA', 'ADT', 'J48', 'LOG',  'NB','RF', 'SVM'), rotation = 300)
 
 #==== Command to change font size of legend ===#
 fontP = FontProperties()
 fontP.set_size('small')
-rects1 = ax.bar(ind, impr_LF_logoptplus_using_clif_bn, width,color ='#bebebe')
+rects1 = ax.bar(ind, avg_impr_LF_logoptplus_using_clif_bn, width,color ='#bebebe')
 ax.plot([0., 6], [0, 0], "k-")
 
 plt.tight_layout()
 #plt.show()
 
-plt.savefig(path+"clif_bn_impr_lf.pdf")
+plt.savefig(path+"clif-bn-avg-impr-lf.pdf")
 
 
 #                       BN  Appraoch                                       #
@@ -136,7 +136,7 @@ plt.savefig(path+"clif_bn_impr_lf.pdf")
 #==========================================================================#
 plt.close()
 
-impr_ra_logoptplus_using_clif_bn=(-3.18, -1.96,  -12.18, -2.6,-7.44, -7.85, -8.54)
+avg_impr_ra_logoptplus_using_clif_bn=(-3.18, -1.96,  -12.18, -2.6,-7.44, -7.85, -8.54)
 #@NOTE: BN is removed because it was not working on sone dataset  because of low probablity
 
 ind = np.arange(7)
@@ -149,17 +149,17 @@ plt.rcParams.update({'font.size': 15})
 ylim(-15,15)
 ax.set_ylabel('Improvement in RA (%)')
 ax.set_xlabel("$CLIF_{BN}$ Classifier")
-ax.set_title('Improvement in RA (%)')
+ax.set_title('Average Improvement in RA (%)')
 ax.set_xticks(ind+width)
 ax.set_xticklabels(('ADA', 'ADT', 'J48', 'LOG',  'NB','RF', 'SVM'), rotation = 300)
 
 #==== Command to change font size of legend ===#
 fontP = FontProperties()
 fontP.set_size('small')
-rects1 = ax.bar(ind, impr_ra_logoptplus_using_clif_bn, width,color ='#bebebe')
+rects1 = ax.bar(ind, avg_impr_ra_logoptplus_using_clif_bn, width,color ='#bebebe')
 ax.plot([0., 6], [0, 0], "k-")
 
 plt.tight_layout()
 #plt.show()
 
-plt.savefig(path+"clif_bn_impr_ra.pdf")
+plt.savefig(path+"clif-bn-avg-impr-ra.pdf")
